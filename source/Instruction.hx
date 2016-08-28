b2c43c1794240d41ded42074aaf1503d85290ecc
Thomas ten Cate <ttencate@gmail.com>
2016-08-28 08:55:26 +0200

    Add descriptions for conditionals

--- source/Instruction.hx
+++ source/Instruction.hx
@@ -7,6 +7,8 @@ class Instruction {
   public var bottomWheel(get, never): Bool;
   public var increment(get, never): Bool;
   public var decrement(get, never): Bool;
+  public var ifZero(get, never): Bool;
+  public var ifNonzero(get, never): Bool;
   public var stitch(get, never): Bool;
   public var move(get, never): Bool;
   public var jump(get, never): Bool;
@@ -28,6 +30,8 @@ class Instruction {
   private function get_bottomWheel(): Bool { return holes[0]; }
   private function get_increment(): Bool { return holes[1] && !holes[2]; }
   private function get_decrement(): Bool { return holes[2] && !holes[1]; }
+  private function get_ifZero(): Bool { return holes[3] && !holes[4]; }
+  private function get_ifNonzero(): Bool { return holes[4] && !holes[3]; }
   private function get_stitch(): Bool { return holes[5]; }
   private function get_move(): Bool { return !holes[6] && (up || down || left || right); }
   private function get_jump(): Bool { return holes[6] && (up || down || left || right); }
@@ -43,69 +47,99 @@ class Instruction {
 
   public function toString() {
     var text = "";
+    var mentionedWheel = false;
     if (increment || decrement) {
       if (increment) {
-        text += " increment";
+        text += "increment";
       } else if (decrement) {
-        text += " decrement";
+        text += "decrement";
       }
-      text += " the value of the";
+      text += " the";
       if (bottomWheel) {
         text += " bottom wheel";
       } else {
         text += " top wheel";
       }
+      mentionedWheel = true;
     }
-    if (stitch) {
+    var conditional = ifZero || ifNonzero;
+    if (conditional) {
       if (text != "") {
-        text += ", then";
+        text += "; if";
+      } else {
+        text += "if";
       }
-      text += " make a stitch";
+      if (mentionedWheel) {
+        text += " it";
+      } else if (bottomWheel) {
+        text += " the bottom wheel";
+      } else {
+        text += " the top wheel";
+      }
+      if (ifZero) {
+        text += " is zero";
+      } else if (ifNonzero) {
+        text += " is not zero";
+      }
+      text += ": ";
+    }
+
+    var textAfterConditional = [];
+    if (stitch) {
+      textAfterConditional.push("make a stitch");
     }
     if (move) {
-      if (text != "") {
-        text += ", then";
-      }
-      text += " move the needle";
-      if (up) {
-        text += " up";
-      } else if (down) {
-        text += " down";
+      var moveText = "move the needle";
+      if (left) {
+        moveText += " left";
+      } else if (right) {
+        moveText += " right";
       }
       if ((up || down) && (left || right)) {
-        text += " and";
+        moveText += " and";
       }
-      if (left) {
-        text += " to the left";
-      } else if (right) {
-        text += " to the right";
+      if (up) {
+        moveText += " up";
+      } else if (down) {
+        moveText += " down";
       }
+      textAfterConditional.push(moveText);
     }
     if (jump) {
-      if (text != "") {
-        text += ", then";
-      }
       if (up || down) {
-        text += " jump";
+        var jumpText = "jump";
         if (up) {
-          text += " one card up";
+          jumpText += " one card up";
         } else if (down) {
-          text += " one card down";
+          jumpText += " one card down";
         }
-      }
-      if ((up || down) && (left || right)) {
-        text += " and";
+        textAfterConditional.push(jumpText);
       }
       if (left) {
-        text += " continue to the left";
+        textAfterConditional.push("continue to the left");
       } else if (right) {
-        text += " continue to the right";
+        textAfterConditional.push("continue to the right");
       }
     }
+
+    for (i in 0...textAfterConditional.length) {
+      var first = i == 0;
+      var last = i == textAfterConditional.length - 1;
+      if (!first) {
+        if (last) {
+          text += conditional ? " and " : ", then ";
+        } else {
+          text += ", ";
+        }
+      }
+      text += textAfterConditional[i];
+    }
+
     text = text.trim();
     if (text == "") {
       text = "do nothing";
     }
+
     text = text.charAt(0).toUpperCase() + text.substring(1);
     return text;
   }
