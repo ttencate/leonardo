febb4dff64819d5f9a71c76d753d464f15a2990c
Thomas ten Cate <ttencate@gmail.com>
2016-08-29 11:44:04 +0200

    Add anigif and wrapper HTML

--- upload.sh
+++ upload.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 haxelib run lime build html5 && \
+  cp -R ./gif ./export/html5/bin/ && \
   rsync -rpltv --delete ./export/html5/bin/ frozenfractal.com:/var/www/leonardo.frozenfractal.com/
